84bd26de9cd754c6c9ba0d617ae6296ab45f2025
johnnydutra23 <johnny.ardutra@gmail.com>
2026-07-03 20:52:41 -0300

    Adicionado um Default Avatar no projeto

--- chats/urls.py
+++ chats/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from chats.views.chats import ChatsView, ChatView
 
+from chats.views.massages import ChatMessagesView, ChatMessageView 
+
 urlpatterns = [
     path('', ChatsView.as_view()),
     path('<int:chat_id>/', ChatView.as_view()),
+    path('<int:chat_id>/messages/', ChatMessagesView.as_view()),
+    path('<int:chat_id>/messages/<int:message_id>/', ChatMessageView.as_view()),
 ]
+
